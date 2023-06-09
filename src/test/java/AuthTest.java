0c23025bccc0b7b499ea2d9bf704b0aee9ce2491
Valeriia Kraft <airkraft20@gmail.com>
2023-06-09 20:22:03 +0700

    Five commit

--- src/test/java/AuthTest.java
+++ src/test/java/AuthTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -24,6 +25,11 @@ public class AuthTest {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
     @AfterEach
     void teardown() {
