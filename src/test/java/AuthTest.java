7dd0efd81e1bc41ae43f8fe3f2e989ee77851c9b
Valeriia Kraft <airkraft20@gmail.com>
2023-06-09 19:29:15 +0700

    first commit

---
+++ src/test/java/AuthTest.java
@@ -0,0 +1,101 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class AuthTest {
+    WebDriver driver;
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        driver = new ChromeDriver();
+    }
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    public void unregisteredUser() {
+        var randomLogin = DataGenerator.getRandomLogin();
+        var randomPassword = DataGenerator.getRandomPassword();
+        $("[data-test-id=login] input").setValue(randomLogin);
+        $("[data-test-id=password] input").setValue(randomPassword);
+        $("[data-test-id=action-login]").click();
+        $("[data-test-id=error-notification]").shouldBe(Condition.visible).shouldHave(Condition.text("Неверно указан логин или пароль"));
+
+    }
+    @Test
+    public void registeredUserWithActiveStatus(){
+        var user = DataGenerator.Registration.getRegisteredUser("active");
+        $("[data-test-id=login] input").setValue(user.getLogin());
+        $("[data-test-id=password] input").setValue(user.getPassword());
+        $("[data-test-id=action-login]").click();
+        $(By.tagName("h2")).shouldBe(Condition.visible).shouldHave(Condition.text("Личный кабинет"));
+    }
+
+    @Test
+    public void registeredAndBlockedUser(){
+        var user = DataGenerator.Registration.getRegisteredUser("blocked");
+        $("[data-test-id=login] input").setValue(user.getLogin());
+        $("[data-test-id=password] input").setValue(user.getPassword());
+        $("[data-test-id=action-login]").click();
+        $("[data-test-id=error-notification]").shouldBe(Condition.visible).shouldHave(Condition.text("Пользователь заблокирован"));
+
+    }
+
+    @Test
+    public void loginWithInvalidUsername(){
+        var user = DataGenerator.Registration.getRegisteredUser("active");
+        var randomLogin = DataGenerator.getRandomLogin();
+        $("[data-test-id=login] input").setValue(randomLogin);
+        $("[data-test-id=password] input").setValue(user.getPassword());
+        $("[data-test-id=action-login]").click();
+        $("[data-test-id=error-notification]").shouldBe(Condition.visible).shouldHave(Condition.text("Неверно указан логин или пароль"));
+    }
+
+    @Test
+    public void loginWithInvalidPassword(){
+        var user = DataGenerator.Registration.getRegisteredUser("active");
+        var randomPassword = DataGenerator.getRandomPassword();
+        $("[data-test-id=login] input").setValue(user.getLogin());
+        $("[data-test-id=password] input").setValue(randomPassword);
+        $("[data-test-id=action-login]").click();
+        $("[data-test-id=error-notification]").shouldBe(Condition.visible).shouldHave(Condition.text("Неверно указан логин или пароль"));
+    }
+
+    @Test
+    public void loginWithEmptyPassword(){
+        var user = DataGenerator.Registration.getRegisteredUser("active");
+        $("[data-test-id=login] input").setValue(user.getLogin());
+        $("[data-test-id=password] input").setValue("");
+        $("[data-test-id=action-login]").click();
+        $("[data-test-id=password]").shouldHave(Condition.text("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    public void loginWithEmptyUsername(){
+        var user = DataGenerator.Registration.getRegisteredUser("active");
+        $("[data-test-id=login] input").setValue("");
+        $("[data-test-id=password] input").setValue(user.getPassword());
+        $("[data-test-id=action-login]").click();
+        $("[data-test-id=login]").shouldHave(Condition.text("Поле обязательно для заполнения"));
+
+    }
+
+}
---
+++ src/test/java/DataGenerator.java
@@ -0,0 +1,69 @@
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import lombok.Value;
+
+import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    private static void sendRequest(RegistrationDto user) {
+        given()
+                .spec(requestSpec)
+                .body(new RegistrationDto(user.getLogin(), user.getPassword(), user.getStatus()))
+                .when()
+                .post("/api/system/users")
+                .then()
+                .statusCode(200);
+    }
+
+    // спецификация нужна для того, чтобы переиспользовать настройки в разных запросах
+    private static final Faker faker = new Faker(new Locale("en"));
+
+    private static RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+
+    public static String getRandomLogin() {
+        return faker.name().firstName();
+    }
+
+    public static String getRandomPassword() {
+        return faker.internet().password();
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static RegistrationDto getUser(String status) {
+            RegistrationDto user = new RegistrationDto(getRandomLogin(), getRandomPassword(), status);
+            return user;
+        }
+
+
+        public static RegistrationDto getRegisteredUser(String status) {
+            RegistrationDto registeredUser = getUser(status);
+            sendRequest(registeredUser);
+            return registeredUser;
+        }
+    }
+
+    @Value
+    public static class RegistrationDto {
+        String login;
+        String password;
+        String status;
+    }
+}
